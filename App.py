81290c06f57310e9c6da591fabe8f79e78f8ed85
kitagaki-at-live2d-com <kitagaki_yuma@live2d.com>
2021-12-12 18:36:02 +0900

    excelファイルを読み取り自動で送信する

--- App.py
+++ App.py
@@ -1,6 +1,9 @@
+import threading
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
+from tkinter import filedialog
 import win32com.client
+import pandas as pd
 
 
 # GUIを提供するクラス
@@ -12,6 +15,10 @@ class App:
     def __init__(self):
         # Mailerのオブジェクト作成
         mailer = Mailer()
+
+        # ファイルマネージャーの初期化
+        filemanager=FileManager()
+        filemanager.initialize()
 
         self.root = tk.Tk()  # ウィンドウ作成
         self.root.geometry(self.WINDOW_SIZE)
@@ -36,16 +43,24 @@ class App:
 
         # 件名
         label_title = tk.Label(self.root, text="件名", font=("normal", 14, "bold"))
-        label_title.place(x=20, y=self.OFFSET + 150)
+        label_title.place(x=20, y=self.OFFSET + 180)
         self.textbox_title = tk.Entry(width=45)
-        self.textbox_title.place(x=80, y=self.OFFSET + 155)
+        self.textbox_title.place(x=80, y=self.OFFSET + 185)
 
         # 本文
         label_body = tk.Label(self.root, text="本文", font=("normal", 14, "bold"))
-        label_body.place(x=20, y=self.OFFSET + 200)
-        self.textbox_body = ScrolledText(self.root, font=("normal", 10), height=15, width=40)
-        self.textbox_body.insert("1.0","{名前} 様")
-        self.textbox_body.place(x=20, y=self.OFFSET + 250)
+        label_body.place(x=20, y=self.OFFSET + 230)
+        self.textbox_body = ScrolledText(self.root, font=("normal", 10), height=15, width=70)
+        # self.textbox_body.insert("1.0", "{名前} 様")
+        self.textbox_body.place(x=20, y=self.OFFSET + 280)
+        # 本文にコピペできるようにする
+        # self.textbox_body.bind("<Control-v>",paste_string)
+        # 本文ファイルを取得するためのボタン設置
+        self.button_get_message_file = tk.Button(self.root,
+                                                 text="インポート",
+                                                 width=10,
+                                                 command=self.import_mail_body)
+        self.button_get_message_file.place(x=100, y=self.OFFSET + 230)
 
         # 送信前確認するかどうかのチェックボックス
         self.checkbutton_value = tk.BooleanVar()
@@ -55,12 +70,16 @@ class App:
                                                      variable=self.checkbutton_value,
                                                      onvalue=True,
                                                      offvalue=False)
-        self.checkbutton_prechecked.place(x=100, y=500)
+        self.checkbutton_prechecked.place(x=100, y=560)
 
         # 送信ボタン
         self.button_send = tk.Button(self.root, text="送信", height=5, width=10,
                                      command=lambda: mailer.send_group_mail(self))
-        self.button_send.place(x=200, y=550)
+        self.button_send.place(x=200, y=600)
+
+        # ペースト機能実装
+        # TODO: 後で実装するかも
+        # self.root.bind("<Control-v>",self.paste_string)
 
     # アプリ起動
     def mainloop(self):
@@ -73,8 +92,8 @@ class App:
         textbox = tk.Entry(width=30)
         textbox.place(x=80, y=self.OFFSET)
         # 宛先名テキストボックス作成
-        textbox_name=tk.Entry(width=30)
-        textbox_name.place(x=350,y=self.OFFSET)
+        textbox_name = tk.Entry(width=30)
+        textbox_name.place(x=350, y=self.OFFSET)
 
         # 宛先テキストボックスのリストに追加
         self.address_textbox_list.append(textbox)
@@ -83,20 +102,45 @@ class App:
 
         self.OFFSET += 30
 
+    # 本文をインポートする
+    def import_mail_body(self):
+        filepath = import_file()
+
+        with open(filepath, encoding="utf-8") as f:
+            str = f.readlines()
+
+        # file = open(filepath, encoding="utf-8")
+
+        # str = file.read()
+        # file.close()
+
+        # 本文のテキストボックスに挿入
+        i = 1
+        for string in str:
+            tmp = "{}.0".format(i)
+            self.textbox_body.insert(tmp, string)
+            i += 1
+
+    # クリップボードからペーストする
+    # TODO: いったん保留で
+    # def paste_string(self):
+    #     body = self.root.clipboard_get()
+    #     self.textbox_body.
+
 
 # メールに関する機能をまとめたクラス
 class Mailer:
     # メールを一斉送信する
     def send_group_mail(self, app):
         # 宛先を取得しリストに格納する
-        address_list = []
-        for address in app.address_textbox_list:
-            address_list.append(address.get())
+        # address_list = []
+        # for address in app.address_textbox_list:
+        #     address_list.append(address.get())
 
         # 宛先名を取得しリストに格納する
-        address_name_list=[]
-        for address_name in app.address_name_textbox_list:
-            address_name_list.append(address_name.get())
+        # address_name_list = []
+        # for address_name in app.address_name_textbox_list:
+        #     address_name_list.append(address_name.get())
 
         # 件名を取得する
         subject = app.textbox_title.get()
@@ -108,23 +152,44 @@ class Mailer:
         is_prechecked = app.checkbutton_value.get()
 
         # 一斉送信
-        i = 0
-        for address in address_list:
-            self.send_mail(address,address_name_list[i],subject, mail_body, is_prechecked)
+        # i = 0
+        # for address in address_list:
+        #     self.send_mail(address, address_name_list[i], subject, mail_body, is_prechecked)
+
+        filemanager = FileManager()
+        filemanager.initialize()
+
+        # 本文を送信先ごとに書き換える
+        for i in range(filemanager.column_number):
+            message = ""
+            # 1行目に社名を追加
+            message += (filemanager.company_list[i] + "\n")
+
+
+            # 2行目に担当者1を追加
+            # 担当者が存在しない場合はスルーする
+            if not pd.isna(filemanager.person1_list[i]):
+                message += filemanager.person1_list[i]
+
+            # 担当者が2人いれば追加する
+            if not pd.isna(filemanager.person2_list[i]):
+                message += ", " + filemanager.person2_list[i]
+            # 「様」を付ける
+            message += " 様\n\n"
+
+            mail_body = message + mail_body
+
+            self.send_mail(filemanager.address_list[i], subject, mail_body, is_prechecked)
 
     # メールを送信する
     def send_mail(self,
                   address,
-                  address_name,
                   subject,
                   body_string,
                   is_prechecked):
         # メールオブジェクトの作成
         outlook = win32com.client.Dispatch("Outlook.Application")
         mail = outlook.CreateItem(0)
-
-        # 本文の{名前}の部分を宛先名に置換する
-        body_string = body_string.replace("{名前}",address_name)
 
         mail.to = address
         mail.subject = subject
@@ -135,3 +200,90 @@ class Mailer:
             mail.display(is_prechecked)
         else:
             mail.Send()
+
+
+# 本文をファイルブラウザで読み込む機能
+def import_file():
+    return filedialog.askopenfilename()
+
+
+# ファイル操作をまとめたクラス(シングルトン)
+class FileManager:
+    # ファイル名
+    ADDRESS_LIST_PATH = "送信先リスト.xlsx"
+    # アドレスの列名
+    ADDRESS_COL_NAME = "メールアドレス"
+    # 社名の列名
+    COMPANY_COL_NAME = "社名"
+    # 担当者1の列名
+    PERSON_1 = "担当者1"
+    # 担当者2の列名
+    PERSON_2 = "担当者2"
+
+    # シングルトンオブジェクト
+    __singleton = None
+    # 初期化済みか否かのフラグ
+    __is_initialized = False
+
+    # 行数
+    column_number = 0
+
+    # アドレスのリスト
+    address_list = []
+    # 社名のリスト
+    company_list = []
+    # 担当者1のリスト
+    person1_list = []
+    # 担当者2のリスト
+    person2_list = []
+
+    # シングルトンを作成して返す
+    def __new__(cls, *args, **kwargs):
+        if cls.__singleton == None:
+            cls.__singleton = super(FileManager,cls).__new__(cls)
+        return cls.__singleton
+
+    def initialize(self):
+        # 初期化済みなら何もしない
+        if self.__is_initialized:
+            return
+
+        # 送信先リストのexcelファイルからデータフレームを作成
+        df = pd.read_excel(self.ADDRESS_LIST_PATH,sheet_name=0,header=0)
+        # 行数の取得
+        self.column_number = len(df)
+
+        # アドレス、社名、担当者1、担当者2のリストを作成
+        for address in df[self.ADDRESS_COL_NAME]:
+            self.address_list.append(address)
+        for company in df[self.COMPANY_COL_NAME]:
+            self.company_list.append(company)
+        for person1 in df[self.PERSON_1]:
+            self.person1_list.append(person1)
+        for person2 in df[self.PERSON_2]:
+            self.person2_list.append(person2)
+
+        # 初期化フラグをオンにする
+        self.__is_initialized = True
+
+    # def __init__(self):
+    #     df = pd.read_excel(self.ADDRESS_LIST_PATH, sheet_name=0, header=0)
+    #
+    #     # 行数取得
+    #     self.column_number = len(df)
+    #
+    #     # アドレス、社名、担当者1, 担当者2のリストを作成
+    #     self.address_list = []
+    #     self.company_name_list = []
+    #     self.person1_list = []
+    #     self.person2_list = []
+    #
+    #     # アドレスのリストに値を追加
+    #     for address in df[self.ADDRESS_COL_NAME]:
+    #         self.address_list.append(address)
+    #     for company in df[self.COMPANY_COL_NAME]:
+    #         self.company_name_list.append(company)
+    #     for person1 in df[self.PERSON_1]:
+    #         self.person1_list.append(person1)
+    #     for person2 in df[self.PERSON_2]:
+    #         self.person2_list.append(person2)
